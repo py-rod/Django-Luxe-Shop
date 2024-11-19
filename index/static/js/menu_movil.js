c86d96c49f98964f29c9c77d8d6ae4db0bff89c9
py-rod <=>
2024-11-19 05:45:10 -0600

    dropdown de perfil arreglado

--- index/static/js/menu_movil.js
+++ index/static/js/menu_movil.js
@@ -1,14 +1,12 @@
 // Función para abrir/cerrar el menú desplegable
 function toggleDropdownMovil() {
-    let dropdown = document.getElementsByClassName("dropdown")[0];
+    let dropdown = document.getElementById("myDropdown");
     dropdown.classList.toggle("open");
 }
 
-// Agregar un controlador de eventos de clic en el documento
 document.addEventListener("click", function (event) {
-    let dropdown = document.getElementsByClassName("dropdown")[0];
-    // Verificar si el clic ocurrió fuera del menú desplegable
-    if (!event.target.closest('.dropdown') && dropdown.classList.contains('open')) {
+    let dropdown = document.getElementById("myDropdown");
+    if (!event.target.closest('.dropdownProfile') && dropdown.classList.contains('open')) {
         dropdown.classList.remove('open');
     }
 });
